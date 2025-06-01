b5254fc7cd83b881a0ef91126b72e96a9d7eacd8
Chaturya Akhila Pallem <84761729+190031241-chaturyaakhilapallem@users.noreply.github.com>
2025-06-01 10:14:19 -0500

    Add files via upload

---
+++ LogicApp/LogicApp/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+Console.WriteLine("Please enter your age: ");
+int Age = int. Parse(Console.ReadLine());
+
+if (Age >= 18)
+{
+    Console.WriteLine("Go party in club!!");
+}
+else if (Age >= 13)
+{
+    Console.WriteLine("Are you here with your parents?: Answer with y or n");
+    string isWithParentsString = Console.ReadLine();
+    if ( isWithParentsString== "y")
+    {
+        Console.WriteLine("Go party in club with your parents!!");
+    }
+    else
+    {
+        Console.WriteLine("No parrty for you.");
+    }
+}
+else
+{
+    Console.WriteLine("Don't go to party.");
+}
+*/
+
+int month=5;
+string monthName;
+
+/*
+if (month==1)
+{
+    monthName = "January";
+}
+else if (month == 2) {
+    monthName = "February";
+}
+else if (month ==3)
+{
+    monthName = "March";
+}
+else
+{
+    monthName = "unknown";
+}
+*/
+
+
+switch (month)
+{
+    case 1:
+        monthName = "January";
+        break;
+    case 2:
+        monthName = "February";
+        break;
+    case 3:
+        monthName = "March";
+        break;
+    default:
+        monthName = "unknown";
+        break;
+
+}
+
+Console.ReadKey();
---
+++ LoopsBasics/LoopsBasics/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// while loop is a pre-test loop
+
+//Random random = new Random();
+//int randomNumber = random.Next(1, 100);
+//int userGuess = 0;
+//int counter = 0;
+//Console.WriteLine("Guess the number I am thinking of between 1 and 100: ");
+
+//while(userGuess !=randomNumber)
+//{
+//    counter++;
+//    Console.WriteLine("Enter you guess: ");
+//    userGuess = int.Parse(Console.ReadLine());
+//    if (userGuess < randomNumber)
+//    {
+//        Console.WriteLine("To Low! Try Again!");
+//    }else if (userGuess > randomNumber){
+//        Console.WriteLine("To High! Try Again!");
+//    }
+//    else
+//    {
+//        Console.WriteLine($"You got it!Congrats! and it is {randomNumber}.It took you {counter} trials to guess.");
+//    }
+//}
+//Console.ReadKey();
+
+int currentScore;
+int sum=0;
+int counter = 0;
+
+// do while loop is a post-test loop
+do
+{
+    Console.WriteLine("Enter your student scores for each subject: ");
+    currentScore = int.Parse(Console.ReadLine());
+    sum = sum + currentScore;
+    counter++;
+} while (counter <=5);
+
+int average = sum / counter;
+Console.WriteLine($"The average score of student is {average}");
+Console.ReadKey();
