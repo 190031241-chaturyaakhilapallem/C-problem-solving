e6652634aba3c22ff8a79c740e5b2c4306c7c30d
Chaturya Akhila Pallem <84761729+190031241-chaturyaakhilapallem@users.noreply.github.com>
2025-06-01 10:13:13 -0500

    Add files via upload

---
+++ QuizApp/QuizApp/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+string question1 = "What is the capital of India?";
+string answer1 = "New Delhi";
+
+string question2 = "what is 2+2?";
+string answer2 = "4";
+
+string question3 = "what color do you get by mixing red and blue?";
+string answer3 = "purple";
+
+int score = 0;
+
+Console.WriteLine("question1");
+string userAnswer1 = Console.ReadLine();
+if (userAnswer1.Trim().ToLower() == answer1.ToLower())
+{
+    Console.WriteLine("Correct!");
+    score++;
+}
+else
+{
+    Console.WriteLine("Not Correct!The correct answer is " + answer1);
+
+}
+
+Console.WriteLine("question2");
+string userAnswer2 = Console.ReadLine();
+if (userAnswer2.Trim().ToLower() == answer2.ToLower())
+{
+    Console.WriteLine("Correct!");
+    score++;
+}
+else
+{
+    Console.WriteLine("Not Correct!The correct answer is " + answer2);
+
+}
+
+Console.WriteLine("question3");
+string userAnswer3 = Console.ReadLine();
+if (userAnswer3.Trim().ToLower() == answer3.ToLower())
+{
+    Console.WriteLine("Correct!");
+    score++;
+}
+else
+{
+    Console.WriteLine("Not Correct!The correct answer is " + answer3);
+
+}
+
+Console.WriteLine($"Congratulations!! Quiz is completed!! Your final score is: {score}/3");
+
+if (score == 3)
+{
+    Console.WriteLine("Excellent! You got all the answers right.");
+}
+else if (score>0)
+{
+    Console.WriteLine("Good job! But try again to get a full score.");
+}
+else
+{
+    Console.WriteLine("Try again please to get some answers right next time...");
+}
+    Console.ReadKey();
---
+++ TextAdventureGame/TextAdventureGame/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Welcome to the Adventure Game!!");
+Console.WriteLine("Enter your Game Character's name: ");
+string characterName = Console.ReadLine();
+Console.WriteLine("Choose your Character class: (Warrior, Wizard, Archer)");
+string characterClass = Console.ReadLine();
+Console.WriteLine($"You {characterName} of character class {characterClass} have found yourself at a dark forest!!");
+Console.WriteLine("Do you want to enter the forest or camp outside?");
+string choice1 = Console.ReadLine();
+if (choice1.ToLower() == "enter")
+{
+    Console.WriteLine("You have bravely entered the forest!!");
+}
+else
+{
+    Console.WriteLine("you decided to camp out of the forest and wait for the morning light!!");
+}
+
+bool gameContinues = true;
+
+if (gameContinues)
+{
+    Console.WriteLine("you have come to an intersection at the road, go left or right?");
+    string direction = Console.ReadLine();
+    if ( direction.ToLower() == "left")
+    {
+        Console.WriteLine("You have found a treasure chest!!");
+        gameContinues = false;
+    }
+    else
+    {
+        Console.WriteLine("You have encountered a wild monster!! Run!!");
+        Console.WriteLine("Either Fight it or Run from it ( fight/ run)");
+        string fightChoice = Console.ReadLine();
+        if (fightChoice.ToLower() == "fight")
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 10);
+            if (randomNumber> 5)
+            {
+                Console.WriteLine("You beat the monster!");
+                if (randomNumber>8)
+                {
+                    Console.WriteLine("the beast dropped a treasure");
+                }
+            }
+            else
+            {
+                Console.WriteLine("you are dead!");
+                Console.WriteLine(" the beat brutally killed you!");
+                gameContinues = false;
+            }
+        }
+
+
+    }
+
+}
+
+Console.ReadKey();
